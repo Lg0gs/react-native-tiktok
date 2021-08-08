93fea7ea47cd0556ffd63acc92187f0780391979
Lg0gs <lgogsadze93@gmail.com>
2021-08-08 13:08:29 +0400

    listen to activity

---
+++ android/src/main/java/com/reactnativetiktok/TikTokEntryActivity.java
@@ -0,0 +1,55 @@
+package com.reactnativetiktok;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+import android.widget.Toast;
+import com.bytedance.sdk.open.aweme.TikTokOpenApiFactory;
+import com.bytedance.sdk.open.aweme.api.TikTokApiEventHandler;
+import com.bytedance.sdk.open.aweme.api.TiktokOpenApi;
+import com.bytedance.sdk.open.aweme.authorize.model.Authorization;
+import com.bytedance.sdk.open.aweme.common.model.BaseReq;
+import com.bytedance.sdk.open.aweme.common.model.BaseResp;
+import com.bytedance.sdk.open.aweme.share.Share;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import java.util.Properties;
+
+public class TikTokEntryActivity extends ReactActivity implements TikTokApiEventHandler {
+    TiktokOpenApi ttOpenApi;
+
+    public void onReq(BaseReq baseReq) {}
+
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        TiktokOpenApi create = TikTokOpenApiFactory.create(this);
+        this.ttOpenApi = create;
+        create.handleIntent(getIntent(), this);
+    }
+
+    public void onResp(BaseResp baseResp) {
+        if (baseResp instanceof Authorization.Response) {
+            Authorization.Response response = (Authorization.Response) baseResp;
+            WritableMap params = Arguments.createMap();
+            params.putInt("status", response.errorCode);
+            params.putString("code", response.authCode);
+            ReactContext ctx = getReactNativeHost().getReactInstanceManager().getCurrentReactContext();
+            ctx.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("onAuthCompleted", params);
+            finish();
+        } else if (baseResp instanceof Share.Response) {
+            Share.Response response = (Share.Response) baseResp;
+            ReactContext ctx = getReactNativeHost().getReactInstanceManager().getCurrentReactContext();
+            ctx.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("onShareCompleted", response.subErrorCode);
+            finish();
+        }
+    }
+
+    public void onErrorIntent(Intent intent) {
+        finish();
+    }
+}
---
+++ android/src/main/java/com/reactnativetiktok/TiktokModule.java
@@ -0,0 +1,89 @@
+package com.reactnativetiktok;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
+
+import com.bytedance.sdk.open.aweme.TikTokOpenApiFactory;
+import com.bytedance.sdk.open.aweme.TikTokOpenConfig;
+import com.bytedance.sdk.open.aweme.api.TiktokOpenApi;
+import com.bytedance.sdk.open.aweme.authorize.model.Authorization;
+import com.bytedance.sdk.open.aweme.base.TikTokMediaContent;
+import com.bytedance.sdk.open.aweme.base.TikTokVideoObject;
+import com.bytedance.sdk.open.aweme.share.Share;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+import java.io.File;
+import java.util.ArrayList;
+
+@ReactModule(name = TiktokModule.NAME)
+public class TiktokModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "Tiktok";
+
+    private void Share(String path) {
+      TiktokOpenApi tiktokOpenApi = TikTokOpenApiFactory.create(getReactApplicationContext());
+      Share.Request request = new Share.Request();
+      request.callerLocalEntry = "com.reactnativetiktok.TikTokEntryActivity";
+      ArrayList<String> mUri = new ArrayList<>();
+      mUri.add(getFileUri(path));
+      TikTokVideoObject videoObject = new TikTokVideoObject();
+      videoObject.mVideoPaths = mUri;
+      TikTokMediaContent content = new TikTokMediaContent();
+      content.mMediaObject = videoObject;
+      request.mMediaContent = content;
+      tiktokOpenApi.share(request);
+    }
+
+    public String getFileUri(String filePath) {
+      ReactContext ctx = getReactApplicationContext();
+      File file = new File(filePath);
+      String authority = ctx.getPackageName() + ".fileprovider";
+      Uri contentUri = FileProvider.getUriForFile(ctx, authority, file);
+      ctx.grantUriPermission("com.zhiliaoapp.musically", contentUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+      ctx.grantUriPermission("com.ss.android.ugc.trill", contentUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+      return contentUri.toString(); // contentUri.toString() should be prefixed with "content://"
+    }
+
+    public TiktokModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
+
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    public void init(String key) {
+      TikTokOpenApiFactory.init(new TikTokOpenConfig(key));
+    }
+
+    @ReactMethod
+    public void auth(Callback callBack) {
+      TiktokOpenApi tiktokOpenApi = TikTokOpenApiFactory.create(getReactApplicationContext());
+      Authorization.Request request = new Authorization.Request();
+      request.scope = "user.info.basic,video.list";
+      request.callerLocalEntry = "com.reactnativetiktok.TikTokEntryActivity";
+      tiktokOpenApi.authorize(request);
+      callBack.invoke("");
+    }
+
+    @ReactMethod
+    public void share(String path, Callback callBack) {
+      Share(path);
+      callBack.invoke("");
+    }
+}
