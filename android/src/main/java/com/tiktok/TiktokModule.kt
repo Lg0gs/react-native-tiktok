b7924de4d0dff91a108f189ab296934b2a0fa2d4
Lg0gs <lgogsadze93@gmail.com>
2025-01-29 12:11:19 +0400

    feat: include modern packages

--- android/src/main/java/com/tiktok/TiktokModule.kt
+++ android/src/main/java/com/tiktok/TiktokModule.kt
@@ -1,22 +1,67 @@
 package com.tiktok
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import com.facebook.react.bridge.ActivityEventListener
+import com.facebook.react.bridge.Callback
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import com.tiktok.open.sdk.auth.AuthApi
+import com.tiktok.open.sdk.auth.AuthRequest
 
 class TiktokModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
+
+  private var authApi: AuthApi? = null
+  private var redirectUrl: String = ""
+  private var callback: Callback? = null
+
+  init {
+    reactContext.addActivityEventListener(object : ActivityEventListener {
+      override fun onNewIntent(intent: Intent?) {
+        authApi?.getAuthResponseFromIntent(intent, redirectUrl)?.let {
+          callback?.invoke(it.authCode)
+        }
+      }
+
+      override fun onActivityResult(activity: android.app.Activity, requestCode: Int, resultCode: Int, data: Intent?) {
+        //
+      }
+    })
+  }
 
   override fun getName(): String {
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
+  fun authorize(redirectURI: String, callback: Callback) {
+    this.redirectUrl = redirectURI
+    this.callback = callback
+
+    val applicationInfo = reactApplicationContext.packageManager.getApplicationInfo(reactApplicationContext.packageName.toString(), PackageManager.GET_META_DATA);
+    val clientKey = applicationInfo.metaData?.getString("TikTokClientKey")
+
+    val request = AuthRequest(
+      clientKey = clientKey!!,
+      scope = "user.info.basic",
+      redirectUri = redirectURI,
+      codeVerifier = "sQ6tdXroCIy746YBgCsNL9DqLJbwE88bBm-PzmB0BTc"
+    )
+
+    if (authApi == null) {
+      authApi = reactApplicationContext.currentActivity?.let {
+        AuthApi(activity = it)
+      }
+    }
+
+    if (authApi != null) {
+      authApi!!.authorize(
+        request = request,
+        authMethod = AuthApi.AuthMethod.TikTokApp
+      )
+    }
   }
 
   companion object {
